69fd9ba2be26690176218e310dfc8c3a53d10365
Lise Ekhorn Johansen <liseej@uio.no>
2024-10-25 15:18:23 +0200

    kinda works

--- src/main/java/com/ass3/protocol/ChordProtocol.java
+++ src/main/java/com/ass3/protocol/ChordProtocol.java
@@ -1,8 +1,11 @@
 package com.ass3.protocol;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -67,7 +70,6 @@ public class ChordProtocol implements Protocol{
 		return this.network;
 	}
 
-
 	/**
 	 * This method builds the overlay network.  It assumes the network object has already been set. It generates indexes
 	 *     for all the nodes in the network. Based on the indexes it constructs the ring and places nodes on the ring.
@@ -123,13 +125,37 @@ public class ChordProtocol implements Protocol{
 	 *     2) interval - [finger[i].start, finger[i+1].start)
 	 *     3) node - first node in the ring that is responsible for indexes in the interval
 	 */
+	@Override
 	public void buildFingerTable() {
-		/*
-		implement this logic
-		 */
+		// Convert topology values to a list for easy access by index
+		List<NodeInterface> nodes = new ArrayList<>(this.network.getTopology().values());
 
+		for (NodeInterface node : nodes) {
+			List<FingerEntry> fingerTable = new ArrayList<>();
+			Integer nodeIndex = node.getId();
+
+			for (int i = 1; i <= m; i++) {
+				int start = (nodeIndex + (int) Math.pow(2, i - 1)) % (int) Math.pow(2, m);
+				int end = (nodeIndex + (int) Math.pow(2, i)) % (int) Math.pow(2, m);
+
+				NodeInterface successor = null;
+				for (NodeInterface n : nodes) {
+					if (n.getId() >= start) {
+						successor = n;
+						break;
+					}
+				}
+
+				if (successor == null) {
+					successor = nodes.get(0); // wrap around the circle
+				}
+
+				fingerTable.add(new FingerEntry(start, end, successor));
+			}
+
+			node.setRoutingTable(fingerTable);
+		}
 	}
-
 
 
 	/**
@@ -141,12 +167,74 @@ public class ChordProtocol implements Protocol{
 	 * @param keyIndex index of the key
 	 * @return names of nodes that have been searched and the final node that contains the key
 	 */
-    @Override
-	public LookUpResponse lookUp(int keyIndex){
-		/*
-		implement this logic
-		 */
-		return null;
+   @Override
+	public LookUpResponse lookUp(int keyIndex) {
+		LinkedHashSet<String> route = new LinkedHashSet<>();
+		int hopCount = 0;
+
+		NodeInterface curr = this.network.getTopology().firstEntry().getValue();
+		route.add(curr.getName());
+
+		// let's pray we don't loop forever
+		while (true) {
+			int currId = curr.getId();
+			NodeInterface successor = curr.getSuccessor();
+			int successorId = successor.getId();
+
+			// are we responsible for this index?
+			if ((currId <= keyIndex && keyIndex < successorId) ||
+				(currId > successorId && (keyIndex >= currId || keyIndex < successorId))) {
+				break; // yay, we are
+			}
+
+			hopCount++;
+
+			List<FingerEntry> fingerTable = (List<FingerEntry>) curr.getRoutingTable();
+			boolean moved = false;
+
+			// let's find the next node to route to
+			for (int i = fingerTable.size() - 1; i >= 0; i--) {
+				FingerEntry candidate = fingerTable.get(i);
+
+				// this is the closest preceding finger
+				if (candidate.successor.getId() < keyIndex) {
+					curr = candidate.successor;
+					moved = true;
+					break;
+				}
+			}
+
+			// guys we didn't find it :( let's ask my successor
+			if (!moved) {
+				curr = successor;
+			}
+
+			route.add(curr.getName());
+		}
+
+		return new LookUpResponse(route, hopCount, curr.getName());
+	}
+
+
+	private static class FingerEntry {
+		int start;
+		int end;
+		NodeInterface successor;
+
+		public FingerEntry(int start, int end, NodeInterface successor) {
+			this.start = start;
+			this.end = end;
+			this.successor = successor;
+		}
+
+        @Override
+        public String toString() {
+			return "FingerEntry{" +
+					"start=" + start +
+					",interval=[" + start + "–" + end + "]"+
+					",successor=" + successor.getName() +
+					'}';
+		        }
 	}
 
 
