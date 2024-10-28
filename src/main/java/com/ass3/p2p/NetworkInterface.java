8c5bcfa2fa46ee2a6a2bd751dee58cfbd4f867c0
Mazunki Hoksaas <rolferen@gmail.com>
2024-10-28 20:09:42 +0100

    consistent style

--- src/main/java/com/ass3/p2p/NetworkInterface.java
+++ src/main/java/com/ass3/p2p/NetworkInterface.java
@@ -4,45 +4,47 @@ import java.util.LinkedHashMap;
 
 public interface NetworkInterface {
 
+	/**
+	 * returns the network object that consists of all the nodes
+	 *
+	 * @return set of nodes (node name, node object)
+	 */
+	LinkedHashMap<String, NodeInterface> getTopology();
 
+	/**
+	 * returns the node object which has the given name
+	 * 
+	 * @param name name of the node object
+	 * @return node object
+	 */
+	NodeInterface getNode(String name);
 
-    /**
-     *  returns the network object that consists of all the nodes
-     *
-     * @return set of nodes (node name, node object)
-     */
-    LinkedHashMap<String, NodeInterface> getTopology();
+	/**
+	 * adds new node to the network object
+	 * 
+	 * @param name name of the node object
+	 * @param node the node object
+	 */
+	void addNode(String name, NodeInterface node);
 
-    /**
-     * returns the node object which has the given name
-     * @param name name of the node object
-     * @return node object
-     */
-    NodeInterface getNode(String name);
+	/**
+	 * removes a node from the network object
+	 * 
+	 * @param name name of the node that needs to be removed
+	 */
+	void removeNode(String name);
 
-    /**
-     * adds new node to the network object
-     * @param name name of the node object
-     * @param node the node object
-     */
-    void addNode(String name, NodeInterface node);
+	/**
+	 * This method prints the network. For each node it prints the corresponding
+	 * information stored in the node
+	 */
+	void printTopology();
 
-    /**
-     * removes a node from the network object
-     * @param name name of the node that needs to be removed
-     */
-    void removeNode(String name);
-
-    /**
-     * This method prints the network. For each node it prints the corresponding information stored in the node
-     */
-    void printTopology();
-
-    /**
-     * returns the number of nodes in the network
-     * @return number of nodes
-     */
-    int getSize();
-
+	/**
+	 * returns the number of nodes in the network
+	 * 
+	 * @return number of nodes
+	 */
+	int getSize();
 
 }
