73bd6ce586d99fbb5cfc601937db5762b40b4080
Mazunki Hoksaas <rolferen@gmail.com>
2024-10-29 12:28:41 +0100

    cleanup

--- src/main/java/com/ass3/ChordProtocolSimulator.java
+++ src/main/java/com/ass3/ChordProtocolSimulator.java
@@ -306,15 +306,6 @@ public class ChordProtocolSimulator {
 		printRing();
 		printNetwork();
 
-		// tests the lookup operation
-		// testLookUp();
-
-		/*
-		 * TODO: implement this logic
-		 */
-		// Look up all the key, print out as required in the Assignment Description
-
-				// Perform lookups and collect results
 		int totalHops = 0;
 		int lookupCount = 0;
 
@@ -322,19 +313,15 @@ public class ChordProtocolSimulator {
 			int keyIndex = entry.getValue();
 			System.err.println(entry.getKey() + " -> " + keyIndex);
 
-			// Perform lookup using the protocol
 			LookUpResponse response = protocol.lookUp(keyIndex);
 			System.err.println("Got response: " + response);
 
-			// Print the lookup response directly using LookUpResponse.toString()
 			System.out.println(response);
 
-			// Accumulate hop count for average calculation
-			totalHops += response.peers_looked_up.size();  // hop count is the size of peers_looked_up
+			totalHops += response.peers_looked_up.size();
 			lookupCount++;
 		}
 
-		// Calculate and print average hop count
 		double averageHopCount = (double) totalHops / lookupCount;
 		System.out.printf("average hop count = %.2f%n", averageHopCount);
 	}
